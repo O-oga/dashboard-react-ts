1c5f16b8c0dbfe1ee6183a9aafce516d9c1e54c5
O_oga <oogaga1996@gmail.com>
2025-11-09 18:33:54 +0100

    Add animation

--- src/components/NaviPanel/SpacePreviewCard/SpacePreviewCard.tsx
+++ src/components/NaviPanel/SpacePreviewCard/SpacePreviewCard.tsx
@@ -15,7 +15,7 @@ function SpacePreviewCard(props : any) {
     
     return (
         <button 
-        className="navi-panel-button button-svg" 
+        className="space-preview-card navi-panel-button button-svg" 
         type="button"
         aria-label={description || t('naviPanel.createNewSpace')}>
             <div className="space-card-icon"><SpaceIcon size={55} color="white" /></div>
